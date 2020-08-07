37f2ae11fbecc8439e349aba3a490cd51e5b6ea6
Dakota Coppage <dakotakcoppage@me.com>
2020-08-07 22:12:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,8 @@ def roll_call_dwarves(dwarves)
   dwarves.each_with_index{|name, index| puts "#{index + 1}. #{name} "}
 end
 
-def summon_captain_planet
+def summon_captain_planet(elements)
+  long_planeteer_calls = []
 end
 
 def long_planeteer_calls# code an argument here
